13e70bf140e42babf9e276e182be3df38ca5064c
Thanapat Chotipun <devpatrick.cho@gmail.com>
2021-03-18 22:07:22 +0700

    [U] main.rs & target config files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![no_std]
 #![no_main]
-use core::panic::PanicInfo;
+#[feature(asm)]
 
+
+use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
 
 #[no_mangle]
+
 pub extern "C" fn _start() -> ! {
     loop {}
 }
